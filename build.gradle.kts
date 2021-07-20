1d4bf4010ee8ce79957edbbce035ca51f15ffda9
ArcticLampyrid <ArcticLampyrid@outlook.com>
2021-07-20 16:17:21 +0800

    build: config publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm") version "1.5.21"
+    `java-gradle-plugin`
+    `maven-publish`
+    signing
+    id("com.gradle.plugin-publish") version "0.14.0"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 group = "com.github.arcticlampyrid"
@@ -13,4 +20,34 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(gradleApi())
     implementation("org.eclipse.jgit:org.eclipse.jgit:5.12.0.202106070339-r")
+}
+
+tasks.getByName<ShadowJar>("shadowJar") {
+    archiveClassifier.set("")
+}
+tasks.getByName("publishPlugins").dependsOn("shadowJar")
+
+pluginBundle {
+    website = "https://github.com/ArcticLampyrid/gradle-git-version"
+    vcsUrl = "https://github.com/ArcticLampyrid/gradle-git-version"
+    tags = listOf("git", "describe", "version")
+}
+
+gradlePlugin {
+    plugins {
+        create("gradleGitVersion") {
+            id = "com.github.arcticlampyrid.gradle-git-version"
+            displayName = "GitVersionPlugin"
+            description = "A plugin to set version based on git info"
+            implementationClass = "com.github.arcticlampyrid.gradle.gitversion.GitVersionPlugin"
+        }
+    }
+}
+
+signing {
+    isRequired = false
+    findProperty("signingKey")?.let {
+        useInMemoryPgpKeys(it.toString(), "")
+    }
+    sign(publishing.publications)
 }
--- src/main/kotlin/com/github/arcticlampyrid/gradle/gitversion/GitVersionPlugin.kt
+++ src/main/kotlin/com/github/arcticlampyrid/gradle/gitversion/GitVersionPlugin.kt
@@ -1,3 +1,5 @@
+package com.github.arcticlampyrid.gradle.gitversion
+
 import org.eclipse.jgit.api.Git
 import org.gradle.api.Plugin
 import org.gradle.api.Project
